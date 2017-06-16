17e8e6fb32a36ed4288c8a375309b22d7858f763
Rıza Gökay Kıvırcıoğlu <riza.gokay@gmail.com>
2017-06-16 12:25:16 +0300

    -Updated tests

--- Mechsoft.ESign.Library.Tests/MainTests.cs
+++ Mechsoft.ESign.Library.Tests/MainTests.cs
@@ -13,37 +13,14 @@ namespace Mechsoft.ESign.Library.Tests
         [TestMethod]
         public void ValidateSignature()
         {
-            var signConfig = new SignatureConfig()
-            {
-                LisansXmlPath = @"config\lisans.xml",
-                PolicyXmlPath = @"config\certval-policy.xml",
-                SertifikaDeposuPath = @"config\SertifikaDeposu.xml"
-
-            };
-
-            var signHelper = new SignatureHelper(signConfig);
-
-            byte[] input =
-                File.ReadAllBytes(@"c:\temp\ss.pdf");
-
-            List<SignatureInfo> infos;
-
-            if (signHelper.IsSignedData(input))
-            {
-                infos = signHelper.CheckSignature(input);
-            }
+           
         }
 
 
         [TestMethod]
         public void ValidateSignatureWithAPI()
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:51978/");
-
-            var bytes = File.ReadAllBytes(@"c:\temp\ss.pdf");
-
-            var response = client.PostAsJsonAsync("REST/GetSignatures", bytes).Result;
+            
 
         }
     }
